fc1dc4c086ff93108007971e4b4131bdacc6ef66
Yurapon Tinlapat <you@example.com>
2024-10-08 15:06:57 +0700

    complete ex1 and ex2

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,18 @@
 // Exercise #1: For Each Function
-
 function forEach(array, operation) {
-  // Start coding here
+  operation(array);
+  return;
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+forEach(employeeSalaries, function (array) {
+  for (let salary of array) {
+    newEmployeeSalaries.push(salary + 5000);
+  }
+  return;
+});
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,14 +2,36 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  return operation(array);
 }
+const allRoomScore = {
+  name: 1,
+  studentScoresRoom1: [90, 40, 67, 80, 100, 15, 86, 12, 99, 67],
+  name: 2,
+  studentScoresRoom2: [78, 98, 23, 15, 40, 12, 40, 67, 80, 100],
+  name: 3,
+  studentScoresRoom3: [67, 80, 100, 15, 12, 40, 67, 80, 100, 67],
+};
 
-const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
-const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
-const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
-
+const isPass = function (array) {
+  let countPass = 0;
+  for (let value of array) {
+    if (value >= 70) {
+      countPass++;
+    } else;
+  }
+  if (countPass >= 5) {
+    return `ผ่านเกณฑ์ ✅`;
+  } else {
+    return `ไม่ผ่านเกณฑ์ ❌`;
+  }
+};
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(allRoomScore.studentScoresRoom1, isPass);
+let scoreRoom2Result = atLeastFive(allRoomScore.studentScoresRoom2, isPass);
+let scoreRoom3Result = atLeastFive(allRoomScore.studentScoresRoom3, isPass);
+
+console.log("นักเรียนห้อง1 " + scoreRoom1Result);
+console.log("นักเรียนห้อง2 " + scoreRoom2Result);
+console.log("นักเรียนห้อง3 " + scoreRoom3Result);
